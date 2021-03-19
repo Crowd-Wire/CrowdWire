565411d06d08f173c99835cfccd32b2301035ef6
BrunosBastos <Brunosb@ua.pt>
2021-03-19 13:37:53 +0000

    database connection working

---
+++ backend/app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings, PostgresDsn
+from typing import Optional
+
+
+class Settings(BaseSettings):
+    
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = 'CrowdWire'
+
+    POSTGRES_SERVER: str = ""
+    POSTGRES_USER: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = "postgresql://postgres:1234@localhost:5432/postgres"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,38 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from typing import Optional
+from sqlalchemy.orm import Session
+
+import sys
+sys.path.append('..')
+
+from app.db import session
 
 
 app = FastAPI()
 
-db = []
-
-class User(BaseModel):
-	name: str
-	age: int
-	money: float
-
-
-
-@app.get("/")
-def index():
-	return {'key': 'value'}
-
-
-@app.get("/users")
-def get_users():
-	return db
-
-@app.post("/users")
-def create_user(user: User):  #only accepts objects that are similar to the User class defined above
-	db.append(user.dict())    #converts to a dictionary so that each one is separated 
-	return db[-1]			  #returns the newly created user
-
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-	db.pop(user_id)			  #deletes the user at the given position
-	return {}				  #returns an empty dict
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-	return db[user_id-1]
+db = session.SessionLocal()
 
