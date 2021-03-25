d737e85c39b3fe1c9cf2a165f09f941bd35f99d8
Pedro <pedrod33@ua.pt>
2021-03-25 15:36:35 +0000

    filters and map card

---
+++ frontend/src/views/Dashboard/sections/MapCard.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import SearchIcon from '@material-ui/icons/Search';
+
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginBottom: "30px",
+    },
+    media: {
+        height: 140,
+    },
+    margin: {
+        margin: theme.spacing(2),
+      },
+}));
+
+export default function MapCard(){
+    const classes = useStyles();
+    return(
+        <>
+            <Col xs={12} sm={6} md={4}>
+                <Card className={classes.root}>
+                <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                    title="Jungle"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Jungle
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    This map was created with the purpose of gathering people to explore the ruins of the lost temple and convey a near life-like experience to users.
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+                <CardActions>
+                <Button size="small" color="primary">
+                    Enter
+                </Button>
+                <Button size="small" color="primary">
+                    More Details
+                </Button>
+                </CardActions>
+                </Card>
+            </Col>
+        </>
+    );
+}
--- frontend/src/views/Dashboard/sections/SearchAllMaps.js
+++ frontend/src/views/Dashboard/sections/SearchAllMaps.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import SearchIcon from '@material-ui/icons/Search';
-
-import Typography from '@material-ui/core/Typography';
+import MapCard from 'views/Dashboard/sections/MapCard.js';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import FormControl from '@material-ui/core/FormControl';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import TuneIcon from '@material-ui/icons/Tune';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 345,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginBottom: "30px",
     },
     media: {
         height: 140,
@@ -30,115 +29,122 @@ const useStyles = makeStyles((theme) => ({
     margin: {
         margin: theme.spacing(2),
       },
+      filter: {
+          margin:"auto",
+      },
+      filterIcon: {
+          fontSize:"2rem",
+          borderRadius:"5px",
+          '&:hover':{
+              backgroundColor:"#45B0AE",
+          },
+      },
+      formControl: {
+        marginRight: theme.spacing(1.5),
+        minWidth: 90,
+      },
 }));
-export default function DashboardContent(){
+
+export default function SearchAllMaps(){
     const classes = useStyles();
+
+    const [typeAccess, setAccess] = React.useState('');
+    const [typeTags, setTags] = React.useState('');
+    const [isOpened, setIsOpened] = useState(false);
+
+    function toggleFilter() {
+        setIsOpened(wasOpened => !wasOpened);
+    }
+    
     const handleChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
       };
-      const [values, setValues] = React.useState({
-        amount: '',
-      });
+
+    function deleteFilters() {
+        setTags('');
+        setAccess('');
+    }
+    const handleAccessibility = (event) => {
+    setAccess(event.target.value);
+    };
+
+    const handleTags = (event) => {
+        setTags(event.target.value);
+    };
+    const [values, setValues] = React.useState({
+    amount: '',
+    });
+
     return(
         <>
-            <Container>
+            <Container style={{overflowX: "hidden"}}>
                 <Row style={{alignContent:"center"}}>
-                    <FormControl fullWidth className={classes.margin} variant="outlined" style={{width:"500px"}}>
-                        <InputLabel htmlFor="outlined-margin-normal">Search</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-amount"
-                            value={values.amount}
-                            onChange={handleChange('amount')}
-                            startAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
-                            labelWidth={60}
-                        />
-                    </FormControl>
+                    <Col xs={12} sm={12} md={6}>
+                        <FormControl fullWidth className={classes.margin} variant="outlined" >
+                            <InputLabel htmlFor="outlined-margin-normal">Search Map</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-amount"
+                                value={values.amount}
+                                onChange={handleChange('amount')}
+                                startAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
+                                labelWidth={90}
+                            />
+                        </FormControl>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} className={classes.filter}>
+                        <Row>
+                                {isOpened ?
+                                <Col xs={10} sm={10} md={10}>
+                                    <FormControl className={classes.formControl} >
+                                        <Select
+                                        value={typeAccess}
+                                        onChange={handleAccessibility}
+                                        displayEmpty
+                                        className={classes.selectEmpty}
+                                        inputProps={{ 'aria-label': 'Without label' }}
+                                        >
+                                        <MenuItem value="">
+                                            <em>Accessibility</em>
+                                        </MenuItem>
+                                        <MenuItem value={'public'}>Public</MenuItem>
+                                        <MenuItem value={'private'}>Private</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <FormControl className={classes.formControl}>
+                                        <Select
+                                        value={typeTags}
+                                        onChange={handleTags}
+                                        displayEmpty
+                                        className={classes.selectEmpty}
+                                        inputProps={{ 'aria-label': 'Without label' }}
+                                        >
+                                        <MenuItem value="">
+                                            <em>Tags</em>
+                                        </MenuItem>
+                                        <MenuItem value={'Leisure'}>Leisure</MenuItem>
+                                        <MenuItem value={'Meetings'}>Meetings</MenuItem>
+                                        <MenuItem value={'Classes'}>Classes</MenuItem>
+                                        <MenuItem value={'Conferences'}>Conferences</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <HighlightOffIcon title="Delete Filters" className={classes.filterIcon} onClick={deleteFilters}/>
+                                </Col>
+                                :          
+                                <Col xs={10} sm={10} md={10}></Col>
+                                }
+                            <Col xs={2} sm={2} md={1} style={{float:"right"}}>
+                                <TuneIcon className={classes.filterIcon} onClick={toggleFilter} style={{float:"right"}}/>
+                            </Col>
+                            <Col xs={0} sm={0} md={1}></Col>
+                        </Row>
+                    </Col>
                 </Row>
-                <br/>
                 <hr/>
                 <Row>
-                    <Col md={4}>
-                        <Card className={classes.root}>
-                        <CardActionArea>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                            title="Jungle"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                            Jungle
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                            This map was created with the purpose of gathering people to explore the ruins of the lost temple and convey a near life-like experience to users.
-                            </Typography>
-                        </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                        <Button size="small" color="primary">
-                            Enter
-                        </Button>
-                        <Button size="small" color="primary">
-                            More Details
-                        </Button>
-                        </CardActions>
-                        </Card>
-                    </Col>
-                    <Col md={4}>
-                        <Card className={classes.root}>
-                        <CardActionArea>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                            title="Jungle"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                            Jungle
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                            This map was created with the purpose of gathering people to explore the ruins of the lost temple and convey a near life-like experience to users.
-                            </Typography>
-                        </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                        <Button size="small" color="primary">
-                            Enter
-                        </Button>
-                        <Button size="small" color="primary">
-                            More Details
-                        </Button>
-                        </CardActions>
-                        </Card>
-                    </Col>
-                    <Col md={4}>
-                        <Card className={classes.root}>
-                        <CardActionArea>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                            title="Jungle"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                            Jungle
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                            This map was created with the purpose of gathering people to explore the ruins of the lost temple and convey a near life-like experience to users.
-                            </Typography>
-                        </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                        <Button size="small" color="primary">
-                            Enter
-                        </Button>
-                        <Button size="small" color="primary">
-                            More Details
-                        </Button>
-                        </CardActions>
-                        </Card>
-                    </Col>
-
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
+                    <MapCard/>
                 </Row>
             </Container>
         </>
