740386505e2243710b2260e02f939e5459e1dd69
Pedro <pedrod33@ua.pt>
2021-03-29 16:53:03 +0100

    fix of the fix

---
+++ frontend/src/consts/gameUI.js
@@ -0,0 +1,6 @@
+export default {
+    MAP: "MAP",
+    MAP_EDITOR: "MAP_EDITOR",
+    MAP_MANAGER: "MAP_MANAGER",
+    SETTINGS: "SETTINGS"
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+
+import { Provider } from 'react-redux';
+
+import store from 'redux/store';
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+
+
+//serviceWorker.unregister();
