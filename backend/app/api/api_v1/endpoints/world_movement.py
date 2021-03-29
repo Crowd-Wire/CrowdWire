f6091502a36c63998b2e78a92c786d0a50377530
Daniel Gomes <dagomes@ua.pt>
2021-03-29 18:01:03 +0100

    disconnection handling

--- backend/app/api/api_v1/endpoints/world_movement.py
+++ backend/app/api/api_v1/endpoints/world_movement.py
@@ -18,22 +18,25 @@ class ConnectionManager:
 
     async def connect(self, world_id: int, websocket: WebSocket):
         await websocket.accept()
+        print("teste", self.active_connections)
+
         if world_id not in self.active_connections:
             self.active_connections[world_id] = [websocket]
         else:
             self.active_connections[world_id].append(websocket)
 
     def disconnect(self, world_id: int, websocket: WebSocket):
-        connections = self.active_connections[world_id].remove(websocket)
-        if not connections:
+        self.active_connections[world_id].remove(websocket)
+        if not self.active_connections[world_id]:
             self.active_connections.pop(world_id)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, world_id: int, message: str):
-        for connection in self.active_connections[world_id]:
-            await connection.send_text(message)
+        if world_id in self.active_connections:
+            for connection in self.active_connections[world_id]:
+                await connection.send_text(message)
 
 
 manager = ConnectionManager()
@@ -41,12 +44,12 @@ manager = ConnectionManager()
 
 @router.websocket('/{world_id}')
 async def world_movement(websocket: WebSocket, world_id: int) -> Any:
-    await manager.connect(websocket)
+    await manager.connect(world_id,websocket)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"World id #{websocket} says: {data}")
+            await manager.broadcast(world_id, f"World id #{websocket} says: {data}")
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{websocket} left the chat")
+        manager.disconnect(world_id, websocket)
+        await manager.broadcast(world_id, f"Client #{websocket} left the chat")
