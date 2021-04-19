cb0a382df1273e84725e88c1f3232fd1284c72a1
Leandro Silva <leandrosilva12@ua.pt>
2021-04-19 22:42:33 +0100

    start game server

---
+++ game_server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    path: '/game',
+    serveClient: false,
+    // below are engine.IO options
+    // pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:4000');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    require('./src/server')(io, socket);
+});
