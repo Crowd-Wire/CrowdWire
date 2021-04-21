752943d0b11c6ed0604199c1cc86639af7240316
BrunosBastos <Brunosb@ua.pt>
2021-04-21 23:04:57 +0100

    flake

--- api/app/test/api/endpoints/tags_test.py
+++ api/app/test/api/endpoints/tags_test.py
@@ -1,12 +1,11 @@
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
 
 from app.main import app
 from unittest.mock import patch
 from unittest import TestCase
 from app.models import Tag
-from app import schemas, models
-from app.api.dependencies import get_current_user, reusable_oauth2
+from app import models
+from app.api.dependencies import get_current_user
 
 client = TestClient(app)
 
@@ -21,11 +20,16 @@ app.dependency_overrides[get_current_user] = override_dependency
 
 class TestTags(TestCase):
     def test_get_all(self):
+        """
+        Expects 200 Ok and confirms the length and content returned
+        """
         with patch("app.crud.crud_tags.CRUDTag.get_all") as mock_post:
             mock_post.return_value = [Tag(name="string"), Tag(name="string2"), Tag(name="string3")]
             response = client.get(
                 "/tags",
             )
+            print(response.json())
             assert response.status_code == 200
             assert mock_post.call_count == 1
             assert len(response.json()) == 3
+            assert response.json()[0]['name'] == "string"
