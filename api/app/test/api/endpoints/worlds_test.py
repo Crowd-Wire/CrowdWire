12c99405695aee3b5eec8f492bdc33b2b0768909
BrunosBastos <Brunosb@ua.pt>
2021-04-22 11:46:43 +0100

    added tests for create and search

--- api/app/test/api/endpoints/worlds_test.py
+++ api/app/test/api/endpoints/worlds_test.py
@@ -5,6 +5,10 @@ from app import models
 from unittest.mock import patch
 from app.api.dependencies import get_current_user
 from app.core.strings import WORLD_NOT_FOUND
+from app.models import Tag
+from app.core import strings
+import json
+
 client = TestClient(app)
 
 
@@ -51,5 +55,124 @@ class TestWorlds(TestCase):
 
             assert response.status_code == 400
             assert response.json()['detail'] == WORLD_NOT_FOUND
+
+    def test_search_worlds_all_worlds(self):
+
+        with patch("app.crud.crud_worlds.CRUDWorld.filter") as mock_get:
+
+            world1 = models.World(world_id=1, world_map=bytes(), creator=1, max_users=1)
+            world2 = models.World(world_id=2, world_map=bytes(), creator=1, max_users=1)
+            mock_get.return_value = [world1, world2]
+
+            response = client.get(
+                '/worlds'
+            )
+
+            assert response.status_code == 200
+            assert len(response.json()) == 2
+
+    def test_search_worlds_by_name_or_description(self):
+
+        with patch("app.crud.crud_worlds.CRUDWorld.filter") as mock_get:
+            world1 = models.World(world_id=1, name="test", world_map=bytes(), creator=1, max_users=1)
+            world2 = models.World(world_id=2, name="", description="test", world_map=bytes(), creator=1, max_users=1)
+            mock_get.return_value = [world1, world2]
+
+            response = client.get(
+                '/worlds/?search=test'
+            )
+
+            assert response.status_code == 200
+            assert len(response.json()) == 2
+            assert "test" in response.json()[0]['name'] or "test" in response.json()[0]["description"]
+            assert "test" in response.json()[1]['name'] or "test" in response.json()[1]["description"]
+
+    def test_search_worlds_by_tags(self):
+
+        with patch("app.crud.crud_worlds.CRUDWorld.filter") as mock_get:
+
+            world1 = models.World(
+                world_id=1,
+                name="test",
+                world_map=bytes(),
+                creator=1,
+                max_users=1,
+                tags=[Tag(name="string")]
+            )
+
+            world2 = models.World(
+                world_id=2,
+                name="",
+                description="test",
+                world_map=bytes(),
+                creator=1,
+                max_users=1,
+                tags=[Tag(name="test")]
+            )
+            mock_get.return_value = [world1, world2]
+
+            response = client.get(
+                '/worlds/?tags=test&tags=string'
+            )
+
+            assert response.status_code == 200
+            assert len(response.json()) == 2
+            assert any([tag['name'] == "string" or tag['name'] == "test" for tag in response.json()[0]['tags']])
+            assert any([tag['name'] == "string" or tag['name'] == "test" for tag in response.json()[1]['tags']])
+
+    def test_create_world_correct_data(self):
+        # TODO: improve this
+        with patch("app.crud.crud_worlds.CRUDWorld.create") as mock_post:
+
+            world = models.World(
+                world_id=1,
+                name="test",
+                world_map=bytes(),
+                creator=1,
+                max_users=1,
+                tags=[Tag(name="string")],
+                public=True,
+                allow_guests=True
+            )
+
+            mock_post.return_value = (world, "")
+
+            response = client.post(
+                "/worlds/",
+                json={
+                    "name": "test",
+                    "world_map": "",
+                    "creator": 1,
+                    "max_users": 1,
+                    "tags": [],
+                    "public": True,
+                    "allow_guests": True
+                }
+            )
+
+            assert response.json()['name'] == world.name
+            assert response.status_code == 200
+
+    def test_create_world_invalid_tags(self):
+        with patch("app.crud.crud_worlds.CRUDWorld.create") as mock_post:
+
+            mock_post.return_value = (None, strings.INVALID_TAG)
+
+            response = client.post(
+                "/worlds/",
+                json={
+                    "world_id": 1,
+                    "name": "test",
+                    "world_map": "",
+                    "creator": 1,
+                    "max_users": 1,
+                    "tags": ['{"name": "aaa"}'],
+                    "public": True,
+                    "allow_guests": True
+                }
+            )
+
+            assert response.status_code == 400
+            assert response.json()['detail'] == strings.INVALID_TAG
 
 
