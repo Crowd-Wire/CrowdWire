f26c90f1b2d5889a4886294518af058ce2f1434b
Leandro Silva <leandrosilva12@ua.pt>
2021-04-24 12:45:27 +0100

    fix circular import

--- api/app/api/api_v1/endpoints/websocket.py
+++ api/app/api/api_v1/endpoints/websocket.py
@@ -1,6 +1,7 @@
 from typing import Any
 
-from app.api.api_v1.endpoints import connection_manager as cm
+from app.websocket.connection_manager import manager
+from app.websocket import websocket_handler as wh
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 from app.core.consts import WebsocketProtocol as protocol
 from loguru import logger
@@ -14,8 +15,8 @@ router = APIRouter()
 @router.websocket(
     "/{world_id}",
 )
-async def websocket_handler(websocket: WebSocket, world_id: str) -> Any:
-    user_id = await cm.manager.connect(world_id, websocket)
+async def world_websocket(websocket: WebSocket, world_id: str) -> Any:
+    user_id = await manager.connect(world_id, websocket)
 
     try:
         while True:
@@ -23,10 +24,10 @@ async def websocket_handler(websocket: WebSocket, world_id: str) -> Any:
             topic = payload['topic']
 
             if topic == protocol.JOIN_PLAYER:
-                await cm.join_player(world_id, user_id, payload)
+                await wh.join_player(world_id, user_id, payload)
 
             elif topic == protocol.PLAYER_MOVEMENT:
-                await cm.send_player_movement(world_id, user_id, payload)
+                await wh.send_player_movement(world_id, user_id, payload)
 
             elif topic == protocol.CHANGE_ROOM:
                 pass
@@ -36,24 +37,24 @@ async def websocket_handler(websocket: WebSocket, world_id: str) -> Any:
                 pass
 
             elif topic in (protocol.JOIN_AS_NEW_PEER, protocol.JOIN_AS_SPEAKER):
-                await cm.join_as_new_peer_or_speaker(world_id, user_id, payload)
+                await wh.join_as_new_peer_or_speaker(world_id, user_id, payload)
 
             elif topic in (
                     protocol.CONNECT_TRANSPORT, protocol.GET_RECV_TRACKS,
                     protocol.CONNECT_TRANSPORT_SEND_DONE, protocol.SEND_TRACK):
-                await cm.handle_transport_or_track(user_id, payload)
+                await wh.handle_transport_or_track(user_id, payload)
 
             elif topic == protocol.CLOSE_MEDIA:
-                await cm.close_media(world_id, user_id, payload)
+                await wh.close_media(world_id, user_id, payload)
 
             elif topic == protocol.TOGGLE_PRODUCER:
-                await cm.toggle_producer(world_id, user_id, payload)
+                await wh.toggle_producer(world_id, user_id, payload)
 
             elif topic == protocol.SPEAKING_CHANGE:
-                await cm.speaking_change(world_id, user_id, payload)
+                await wh.speaking_change(world_id, user_id, payload)
 
             else:
                 logger.error(f"Unknown topic \"{topic}\"")
     except WebSocketDisconnect:
-        cm.manager.disconnect(world_id, user_id)
+        manager.disconnect(world_id, user_id)
         # TODO: disconnect from room
--- api/app/websocket/connection_manager.py
+++ api/app/websocket/connection_manager.py
@@ -2,10 +2,8 @@ from typing import Dict, List
 
 from fastapi import WebSocket
 from loguru import logger
-import json
 
 from app.core.consts import WebsocketProtocol as protocol
-from app.rabbitmq import rabbit_handler
 
 
 class ConnectionManager:
@@ -100,93 +98,6 @@ class ConnectionManager:
             logger.error(
                 f"Error when trying to broadcast to World {world_id}, Room {room_id}"
             )
-
-
-async def join_player(world_id, user_id, payload):
-    room_id = payload['room_id']
-    position = payload['position']
-    await manager.connect_room(world_id, room_id, user_id, position)
-
-
-async def send_player_movement(world_id, user_id, payload):
-    room_id = payload['room_id']
-    velocity = payload['velocity']
-    position = payload['position']
-    await manager.broadcast(
-        world_id, room_id,
-        {'topic': protocol.PLAYER_MOVEMENT, 'user_id': user_id, 'velocity': velocity, 'position': position},
-        user_id
-    )
-
-
-async def join_as_new_peer_or_speaker(world_id, user_id, payload):
-    """
-    join-as-new-peer:
-        create a room if it doesnt exit and add that user to that room
-        then media server returns receive transport options to amqp
-        this should only allow to receive audio
-    join-as-speaker:
-        this does the same as above, except it allows
-        the user to speak, so, it returns two kinds of transport,
-        one for receiving and other for sending
-    """
-    room_id = payload['d']['roomId']
-    payload['d']['peerId'] = user_id
-
-    if room_id in manager.connections[world_id][room_id]:
-        if user_id not in manager.connections[world_id][room_id]:
-            manager.connections[world_id][room_id].append(user_id)
-    else:
-        manager.connections[world_id][room_id] = [user_id]
-
-    await rabbit_handler.publish(json.dumps(payload))
-
-
-async def handle_transport_or_track(user_id, payload):
-    payload['d']['peerId'] = user_id
-    await rabbit_handler.publish(json.dumps(payload))
-
-
-async def close_media(world_id, user_id, payload):
-    room_id = payload['d']['roomId']
-    payload['d']['peerId'] = user_id
-
-    # close in media server producer
-    await rabbit_handler.publish(json.dumps(payload))
-
-    # broadcast for peers to close this stream
-    if user_id in manager.connections[world_id][room_id]:
-        await manager.broadcast(world_id, room_id,
-                                {'topic': protocol.CLOSE_MEDIA,
-                                 'peerId': user_id},
-                                user_id)
-
-
-async def toggle_producer(world_id, user_id, payload):
-    room_id = payload['d']['roomId']
-    kind = payload['d']['kind']
-    pause = payload['d']['pause']
-    payload['d']['peerId'] = user_id
-    # pause in media server producer
-    await rabbit_handler.publish(json.dumps(payload))
-    # broadcast for peers to update UI toggle buttons
-    if user_id in manager.connections[world_id][room_id]:
-        await manager.broadcast(world_id, room_id,
-                                {'topic': protocol.TOGGLE_PEER_PRODUCER,
-                                 'peerId': user_id,
-                                 'kind': kind,
-                                 'pause': pause},
-                                user_id)
-
-
-async def speaking_change(world_id, user_id, payload):
-    room_id = payload['d']['roomId']
-    value = payload['d']['value']
-    logger.info(user_id)
-    logger.info(manager.connections[world_id][room_id])
-    if user_id in manager.connections[world_id][room_id]:
-        await manager.broadcast(world_id, room_id,
-                                {'topic': protocol.ACTIVE_SPEAKER, 'peerId': user_id, 'value': value}, user_id)
 
 
 manager = ConnectionManager()
