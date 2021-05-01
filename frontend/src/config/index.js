cd2c06c3d91a9d02b24636d482a26715ecd0ea39
Daniel Gomes <dagomes@ua.pt>
2021-05-01 11:06:02 +0100

    initial fixture

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,2 +1,11 @@
-export const WS_BASE = "ws://localhost:8000";
-export const API_BASE = "http://localhost:8000/";
+if (process.env.RUNNING_MODE && process.env.RUNNING_MODE == 'production'){
+    HOST = process.env.API_SERVICE_NAME || 'localhost'
+    WS_BASE = "ws://{$HOST}:8000"
+    API_BASE = "http://{$HOST}:8000/"
+}
+else {
+    WS_BASE = "ws://localhost:8000";
+    API_BASE = "http://localhost:8000/";
+}
+export const WS_BASE;
+export const API_BASE;
