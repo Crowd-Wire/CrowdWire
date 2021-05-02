4b805114c248fe4a0cd73334ba5856d0dbdd6b00
Daniel Gomes <dagomes@ua.pt>
2021-05-02 21:02:33 +0100

    fixtured done.. for now

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,11 +1,14 @@
-if (process.env.RUNNING_MODE && process.env.RUNNING_MODE == 'production'){
-    HOST = process.env.API_SERVICE_NAME || 'localhost'
-    WS_BASE = "ws://{$HOST}:8000"
-    API_BASE = "http://{$HOST}:8000/"
+export let WS_BASE = null;
+export let API_BASE = null;
+console.log(window._env_);
+console.log(window._env_.REACT_APP_RUNNING_MODE);
+if (window._env_.REACT_APP_RUNNING_MODE != null && window._env_.REACT_APP_RUNNING_MODE == 'production'){
+    let HOST = window._env_.REACT_APP_API_IP
+    console.log("host" + HOST)
+    WS_BASE = `ws://${HOST}`
+    API_BASE = `http://${HOST}/api/v1`
 }
 else {
     WS_BASE = "ws://localhost:8000";
     API_BASE = "http://localhost:8000/";
 }
-export const WS_BASE;
-export const API_BASE;
