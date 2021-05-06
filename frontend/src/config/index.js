30e00b62760ab21d12db8941a39a13f7ad1f9dd1
Mário Silva <47184969+MarioCSilva@users.noreply.github.com>
2021-05-07 00:47:22 +0100

    Update index.js

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,10 +2,9 @@ export let WS_BASE = null;
 export let API_BASE = null;
 export let URL_BASE = null;
 console.log(window.REACT_APP_RUNNING_MODE);
-console.log(window.REACT_APP_API_IP);
 if (window.REACT_APP_RUNNING_MODE != null && window.REACT_APP_RUNNING_MODE == 'production'){
     let HOST = "crowdwire.duckdns.org"
-    console.log("host" + HOST)
+    console.log("host " + HOST)
     WS_BASE = `wss://${HOST}/api/v1`
     API_BASE = `https://${HOST}/api/v1/`
     URL_BASE = `https://${HOST}/`
