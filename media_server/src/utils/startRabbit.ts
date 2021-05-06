cb57de8208296a354bd534a31c67bf200c18ca46
DanielGomes14 <48451636+DanielGomes14@users.noreply.github.com>
2021-05-06 13:43:07 +0100

    Update startRabbit.ts

--- media_server/src/utils/startRabbit.ts
+++ media_server/src/utils/startRabbit.ts
@@ -146,11 +146,11 @@ export let send = <Key extends keyof OutgoingMessageDataMap>(
 export const startRabbit = async (handler: HandlerMap) => {
   console.log(
     "trying to connect to: ",
-    process.env.RABBITMQ_URL || "amqp://user:bitnami@localhost"
+    process.env.RABBITMQ_URL || "amqp://user:bitnami@crowdwire-rabbitmq:5672"
   );
   let conn: Connection;
   try {
-    conn = await amqp.connect(process.env.RABBITMQ_URL || "amqp://user:bitnami@localhost");
+    conn = await amqp.connect(process.env.RABBITMQ_URL || "amqp://user:bitnami@crowdwire-rabbitmq:5672");
   } catch (err) {
     console.error("Unable to connect to RabbitMQ: ", err);
     setTimeout(async () => await startRabbit(handler), retryInterval);
