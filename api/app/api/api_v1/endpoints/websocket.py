4ced090c6a74e59351b3c6e2e470b4e1dac27487
mario <mariosilva@ua.pt>
2021-05-07 11:59:26 +0100

    merge

--- api/app/api/api_v1/endpoints/websocket.py
+++ api/app/api/api_v1/endpoints/websocket.py
@@ -13,17 +13,6 @@ Class used to Manage the WebSocket Connections, to each World
 """
 router = APIRouter()
 
-HEART_BEAT_INTERVAL = 5
-
-async def is_websocket_active(ws: WebSocket) -> bool:
-    try:
-        await asyncio.wait_for(ws.send_json({'type': 'ping'}), HEART_BEAT_INTERVAL)
-        message = await asyncio.wait_for(ws.receive_json(), HEART_BEAT_INTERVAL)
-        assert message['type'] == 'pong'
-    except BaseException:
-        return False
-    return True
-
 
 @router.websocket(
     "/{world_id}",
