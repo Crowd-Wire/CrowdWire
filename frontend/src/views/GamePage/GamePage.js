aa7ecb5addd43974e1303b12cf62459e847ce33a
Leandro Silva <leandrosilva12@ua.pt>
2021-05-15 22:19:50 +0100

    frontend

--- frontend/src/views/GamePage/GamePage.js
+++ frontend/src/views/GamePage/GamePage.js
@@ -5,20 +5,64 @@ import GameDrawer from 'components/GameDrawer/GameDrawer';
 import Phaser from "./Sections/Phaser.js";
 
 import styles from "assets/jss/my-kit-react/views/gamePageStyle.js";
-// import WorldService from "../../services/WorldService";
+import WorldService from "../../services/WorldService";
+import { toast } from 'react-toastify';
+import logo from '../../assets/crowdwire_white_logo.png';
+import useWorldUserStore from '../../stores/useWorldUserStore';
 
 const GamePage = (props) => {
 
   const { classes } = props;
+  const [loading, setLoading] = useState(1)
+
+  const toast_props = {
+    position: toast.POSITION.TOP_RIGHT,
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    progress: undefined,
+  }
+
+  useEffect(() => {
+    WorldService.join_world(1)
+    .then((res) => res.json())
+    .then(
+      (res) => {
+        console.log(res)
+        if(res.detail){
+          toast.dark(
+            <span>
+              <img src={logo} style={{height: 22, width: 22,display: "block", float: "left", paddingRight: 3}} />
+              {res.detail}
+            </span>
+          ,toast_props);
+        }
+        else {
+          useWorldUserStore.getState().joinWorld(res);
+          console.log(useWorldUserStore.getState());
+          setLoading(0);
+        }
+      }
+    )
+  }, [])
   
   return (
-    <div className={classes.wrapper}>
-      <GameDrawer />
-      <div className={classes.gameWindow}>
-        {/* Game */}
-        <Phaser />
-      </div>
-    </div>
+    <>
+      {loading ? 
+        ''
+      : 
+        <div className={classes.wrapper}>
+          <GameDrawer />
+          <div className={classes.gameWindow}>
+            {/* Game */}
+            <Phaser />
+          </div>
+        </div>
+      }
+    </>
   );
 }
 
