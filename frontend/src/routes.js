fe0c58086e5e0bdd53ff5894ca6220f811efe867
leand12 <60890792+leand12@users.noreply.github.com>
2021-05-25 22:44:18 +0100

    Revert "Feature frontend events stats"

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -27,7 +27,6 @@ import InviteJoinPage from "views/InvitePage/InviteJoinPage.js";
 import DashboardContent from "views/DashWorldDetails/sections/DashboardContent.js";
 import SearchAllMaps from "views/DashSearch/sections/SearchAllMaps.js";
 import AdminWorld from 'views/AdminWorld/AdminWorld.js';
-import AdminStatistics from 'views/AdminStatistics/AdminStatistics.js'
 /**
  * Public and protected routes list 
  * Based on https://stackoverflow.com/questions/62384395/protected-route-with-react-router-v6
@@ -96,7 +95,6 @@ const routes = (token, guest_uuid) => [
 		element: token ? <AdminLayout /> : <Navigate to="/login" />,
 		children: [
             { path: "/worlds", element: <AdminWorld /> },
-			{ path: "/statistics", element: <AdminStatistics/>}
 		],
 	},
     { path: "*", element: <NotFound /> },
--- frontend/src/utils/parsetoLocalDate.js
+++
@@ -1,6 +0,0 @@
-export default function  parseDate (date) {
-    let tempDate = new Date(date);
-    let newDate = new Date(tempDate.getTime() - tempDate.getTimezoneOffset()*60*1000);
-    return newDate.toString().replace(/GMT.*/g,"")
-
-}
