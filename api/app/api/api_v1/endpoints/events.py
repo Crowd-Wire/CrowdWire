b7bdf3545288e3a8c77fededd765c6f20e9e342e
Daniel Gomes <dagomes@ua.pt>
2021-05-25 23:03:33 +0100

    done listing

--- api/app/api/api_v1/endpoints/events.py
+++ api/app/api/api_v1/endpoints/events.py
@@ -1,12 +1,12 @@
 import datetime
 from typing import Union, List, Optional
-
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app import models, schemas, crud
 from app.api import dependencies as deps
 from app.core import strings
+from app.core.consts import WebsocketProtocol as protocol
 from app.utils import is_guest_user
 
 router = APIRouter()
@@ -17,7 +17,7 @@ def get_world_events(
         world_id: int,
         db: Session = Depends(deps.get_db),
         user: Union[models.User, schemas.GuestUser] = Depends(deps.get_current_user),
-        event_type: Optional[str] = None,
+        event_type: List[str] = Query(None),
         user_id: Optional[int] = None,
         order_desc: bool = True,
         start_date: datetime.datetime = None,
