03d013b582df6e4545c9fde0d2a5dd2627a5f13f
Daniel Gomes <dagomes@ua.pt>
2021-05-25 23:13:21 +0100

    idk conflicts..

--- api/app/api/api_v1/endpoints/events.py
+++ api/app/api/api_v1/endpoints/events.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import Union, List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app import models, schemas, crud
@@ -17,7 +17,7 @@ def get_world_events(
         world_id: int,
         db: Session = Depends(deps.get_db),
         user: Union[models.User, schemas.GuestUser] = Depends(deps.get_current_user),
-        event_type: Optional[str] = None,
+        event_type: List[str] = Query(None),
         user_id: Optional[int] = None,
         order_desc: bool = True,
         start_date: datetime.datetime = None,
