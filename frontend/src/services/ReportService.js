939ddc8fdb43eeee89d996b6c2016f0a2ada44da
BrunosBastos <Brunosb@ua.pt>
2021-05-28 00:07:56 +0100

    fix

--- frontend/src/services/ReportService.js
+++ frontend/src/services/ReportService.js
@@ -10,7 +10,7 @@ class ReportService{
             headers: {
                 "Authorization" : "Bearer "+ AuthenticationService.getToken()
             },
-            body: JSON.stringify({reported: world_id, comment: comment})
+            body: JSON.stringify({comment: comment})
         })
     }
 }
