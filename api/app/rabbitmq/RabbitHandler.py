508dbec79eb1b448b818e71130785ce1e75c2e26
BrunosBastos <Brunosb@ua.pt>
2021-05-27 11:00:02 +0100

    create queue and refactor

--- api/app/rabbitmq/RabbitHandler.py
+++ api/app/rabbitmq/RabbitHandler.py
@@ -43,6 +43,14 @@ async def on_message(message: IncomingMessage) -> None:
         else:
             logger.error(f"Unknown topic \"{topic}\"")
 
+async def on_replica_message(message: IncomingMessage) -> None:
+    async with message.process():
+        msg = message.body.decode()
+        msg = json.loads(msg)
+        users = msg['users']
+        logger.debug(users)
+        for user_id in users:
+            await manager.send_personal_message(msg, user_id, False)
 
 class RabbitHandler:
     def __init__(self):
@@ -51,11 +59,13 @@ class RabbitHandler:
         self.channel_pool = None
         self.queue_to_send = settings.RABBITMQ_SENDING_QUEUE
         self.queue_to_receive = settings.RABBITMQ_RECEIVING_QUEUE
+        self.replicas_queue = settings.RABBITMQ_REPLICA_QUEUE
 
     async def start_pool(self):
         self.connection_pool = Pool(self.get_connection, max_size=2)
         self.channel_pool = Pool(self.get_channel, max_size=10)
         await self.consume()
+        await self.consume_replica()
 
     async def get_connection(self):
         return await connect_robust(self.uri)
@@ -83,5 +93,20 @@ class RabbitHandler:
             )
             await queue.consume(on_message)
 
+    async def publish_replica(self, message: str) -> None:
+        async with self.channel_pool.acquire() as channel:  # type: Channel
+            await channel.default_exchange.publish(
+                Message(message.encode()),
+                self.replicas_queue,
+            )
+            logger.info("Published message to Channel %r" % channel)
+
+    async def consume_replica(self) -> None:
+        async with self.channel_pool.acquire() as channel:
+            await channel.set_qos(10)
+            queue = await channel.declare_queue(
+                self.replicas_queue, durable=True
+            )
+            await queue.consume(on_replica_message)
 
 rabbit_handler = RabbitHandler()
