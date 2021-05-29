fcc27ca39467b07d4d7eeb6f5b8293090f3e4b52
Leandro Silva <leandrosilva12@ua.pt>
2021-05-30 00:22:32 +0100

    start world editor store

---
+++ frontend/src/stores/useWorldEditorStore.ts
@@ -0,0 +1,55 @@
+import create from "zustand";
+import { combine } from "zustand/middleware";
+
+
+enum PaintToolType {
+    STAMP = "STAMP",
+    FILL = "FILL",
+    SHAPE = "SHAPE",
+    ERASE = "ERASE",
+}
+
+interface Layer {
+    visible: boolean;
+}
+
+interface PaintTool {
+    type: PaintToolType;
+}
+
+const useWorldEditorStore = create(
+    combine(
+        {   
+            ready: false,
+            activeLayer: null,
+            layers: {} as Record<string, Layer>,
+            paintTool: {} as PaintTool,
+        },
+        (set) => ({
+            setReady: () => {
+                return set(() => {
+                    return { ready: true };
+                });
+            },
+            setPaintTool: (settings: PaintTool) => {
+                return set((s) => {
+                    return { paintTool: {...s.paintTool, ...settings} };
+                });
+            },
+            setActiveLayer: (activeLayer: string) => {
+                return set(() => {
+                    return { activeLayer };
+                });
+            },
+            setLayer: (name: string, settings: Layer) => {
+                return set((s) => {
+                    const layers = {...s.layers};
+                    layers[name] = {...layers[name], ...settings};
+                    return { layers };
+                });
+            },
+        })
+    )
+);
+
+export default useWorldEditorStore;
