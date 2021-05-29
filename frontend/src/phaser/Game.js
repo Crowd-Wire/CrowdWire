99992a8901d8df68a1f0a4fa9c4b94feb41ee504
Leandro Silva <leandrosilva12@ua.pt>
2021-05-29 01:22:43 +0100

    gameconfig according to scene 2

--- frontend/src/phaser/Game.js
+++ frontend/src/phaser/Game.js
@@ -10,38 +10,6 @@ const gameConfig = {
 
   // prevent the blur of the textures when scaled
   pixelArt: true, 
-  zoom: 2,
-
-  // width: window.innerWidth,
-  // height: window.innerHeight,
-
-  scale: {
-      parent: "game-container",
-
-      // mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
-      // mode: Phaser.DOM.RESIZE,
-
-      // mode: Phaser.DOM.ENVELOP,
-      // autoCenter: Phaser.DOM.CENTER_BOTH,
-
-      //mode: Phaser.Scale.FIT,
-      // mode: Phaser.Scale.RESIZE,
-      // mode: Phaser.Scale.NONE,
-
-      //mode: Phaser.DOM.FIT,
-      // autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-
-      width: '100%',
-      height: '100%',
-      // min: {
-      //   width: 800,
-      //   height: 800
-      // },
-      max: {
-        width: window.innerWidth,
-        height: window.innerHeight
-      }
-  },
 
   // autoResize: true,//n faz nasda
   physics: {
@@ -57,11 +25,25 @@ const gameConfig = {
 
 const sceneConfig = {
   GameScene: {
-    mode: Phaser.Scale.RESIZE,
+    zoom: 2,
+    scale: {
+      parent: "game-container",
+      mode: Phaser.Scale.RESIZE,
+      width: '100%',
+      height: '100%',
+      max: {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+    },
     scene: [BootScene, GameScene],
   },
   WorldEditorScene: {
-    mode: Phaser.Scale.NONE,
+    zoom: 1,
+    scale: {
+      parent: "game-container",
+      mode: Phaser.Scale.NONE
+    },
     scene: [BootScene, WorldEditorScene],
   }
 }
