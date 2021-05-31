08e29ce73a56cef284ad8e56a8b91dd70dd9787f
mario <mariosilva@ua.pt>
2021-05-31 13:55:56 +0100

    fixed cards display

--- frontend/src/components/MapCard/MapCard.js
+++ frontend/src/components/MapCard/MapCard.js
@@ -46,7 +46,7 @@ export default function MapCard(props){
     const classes = useStyles();
     return(
         <>
-            <Col xs={12} sm={6} md={4} style={{margin: 'auto'}}>
+            <Col xs={12} sm={12} md={6} lg={4} style={{height: 380, marginLeft: 'auto', marginRight: 'auto'}}>
                 <Card className={classes.root}>
                     <CardActionArea onClick={routeChange}>
                         <CardMedia
