fd0eaa819ae09f71039a9c3a5dd7d98d35ad48a6
BrunosBastos <Brunosb@ua.pt>
2021-06-06 11:07:33 +0100

    finish email confirm in frontend

--- frontend/src/views/ConfirmEmail/ConfirmEmail.js
+++ frontend/src/views/ConfirmEmail/ConfirmEmail.js
@@ -2,10 +2,27 @@ import React from 'react';
 import AuthenticationService from 'services/AuthenticationService.js';
 import { Card, Box } from '@material-ui/core';
 import image from "assets/img/bg8.png";
+import {useNavigate} from 'react-router-dom';
+import { toast } from 'react-toastify';
+import logo from 'assets/crowdwire_white_logo.png';
+import ErrorIcon from '@material-ui/icons/Error';
+
+const toast_props = {
+    position: toast.POSITION.TOP_CENTER,
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    progress: undefined,
+  }
+  
 
 export default function ConfirmEmail() {
 
     const [verified, setVerified] = React.useState(0)
+    const navigate = useNavigate();
 
     React.useEffect(() => {
 
@@ -22,8 +39,17 @@ export default function ConfirmEmail() {
                 }
 
             }).then((res) => {
-                if (res)
+                if (res) {
                     AuthenticationService.setToken(res, "REGISTER");
+                    toast.success(
+                        <span>
+                          <img src={logo} style={{height: 22, width: 22,display: "block", float: "left", paddingRight: 3}} />
+                          Your account is now verified!
+                        </span>
+                        , toast_props
+                      );
+                    navigate("/dashboard/search/public");
+                }
             })
 
     }, [])
@@ -43,8 +69,9 @@ export default function ConfirmEmail() {
                     :
 
                     <Box width="70%" height="50%">
-                        <Card style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
-                            <h3>Verification Link not Valid.</h3>
+                        <Card style={{ width: '100%', height: '100%',  justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+                            <ErrorIcon style={{color: 'red', fontSize: '10rem', flexGrow: '1', display: 'flex', justifyContent: 'center'}} />
+                            <h3 style={{flexGrow: '3', display: 'flex', justifyContent: 'left'}}>Verification Link could not be validated.</h3>
                         </Card>
                     </Box>
 
