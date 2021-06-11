f2f39537b1dbe3f4374ae11000f266fdcf0e606c
Pedro <pedrod33@ua.pt>
2021-06-11 11:52:13 +0100

    layout

--- frontend/src/views/NotFound/NotFound.js
+++ frontend/src/views/NotFound/NotFound.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Button from "components/CustomButtons/Button.js";
+import Typography from "@material-ui/core/Typography";
+import Row from 'react-bootstrap/Row';
+import image from "assets/img/desert_404.jpg";
 export default class NotFound extends React.Component {
     render () {
-        return "NOT FOUND :(";
+        return(
+            <>
+                <div style={{ backgroundImage: "url(" + image + ")", height:"100%", backgroundSize:"cover"}}>
+                    <Row style={{marginLeft:"auto", marginRight:"auto"}}>
+                        <Typography variant="h2" style={{marginLeft:"auto", marginRight:"auto", marginTop:"20%"}}><em>There's nothing here...</em></Typography>
+                    </Row>
+                    <Row style={{marginLeft:"auto", marginRight:"auto"}}>
+                        <Button style={{marginLeft:"auto", marginRight:"auto", marginTop:"10%"}}>Back to dashboard</Button>
+                    </Row>
+                </div>
+            </>
+        );
     }
 }
